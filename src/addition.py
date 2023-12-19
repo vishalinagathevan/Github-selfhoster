4ccd40c714be6b1efce250b118e325e7913ef7fd
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-19 16:21:38 +0530

    workflow file added

---
+++ src/addition.py
@@ -0,0 +1,8 @@
+# Store input numbers
+num1 = 10.5
+num2 = 20.3
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
